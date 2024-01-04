9b633b73ba0d953ff2d502b0df4bef111dd08fa1
Natalia Amada <nataliaxamada@gmail.com>
2024-01-04 16:00:39 -0500

    add border

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ canvas.height = quiltSize;
 function drawQuilt() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // Draw border
+  ctx.strokeStyle = "#000000";
+  ctx.strokeRect(0, 0, quiltSize, quiltSize);
+
   for (let x = 0; x < quiltSize; x += 80) {
     for (let y = 0; y < quiltSize; y += 80) {
       drawSquare(x, y);
@@ -16,35 +20,15 @@ function drawQuilt() {
 }
 
 function drawSquare(x, y) {
-  ctx.fillStyle = "#FFFFFF";
-  ctx.fillRect(x, y, 80, 80);
-
-  const corner = Math.floor(Math.random() * 4);
-  drawHalfCircle(x, y, corner);
-}
-
-function drawHalfCircle(x, y, corner) {
-  ctx.beginPath();
   const color = getRandomColor();
   ctx.fillStyle = color;
+  ctx.fillRect(x, y, 80, 80);
 
-  switch (corner) {
-    case 0:
-      ctx.arc(x + 80, y + 80, 40, 0, Math.PI);
-      break;
-    case 1:
-      ctx.arc(x, y + 80, 40, Math.PI / 2, Math.PI * 1.5);
-      break;
-    case 2:
-      ctx.arc(x + 80, y, 40, -Math.PI / 2, Math.PI / 2);
-      break;
-    case 3:
-      ctx.arc(x, y, 40, Math.PI, Math.PI * 2);
-      break;
-    default:
-      break;
-  }
-
+  // Draw a circle in the center of the square
+  const centerX = x + 40;
+  const centerY = y + 40;
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, 30, 0, Math.PI * 2);
   ctx.fill();
 }
 
