aa97dbef53096d0084a572085ccbb3b3ad75b9be
Natalia Amada <nataliaxamada@gmail.com>
2024-01-04 16:29:09 -0500

    tweaking possible shapes

--- script.js
+++ script.js
@@ -9,11 +9,6 @@ canvas.height = quiltSize;
 function drawQuilt() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Draw border
-  ctx.lineWidth = 4;
-  ctx.strokeStyle = "#000000"; // Border color is black
-  ctx.strokeRect(2, 2, quiltSize - 4, quiltSize - 4);
-
   for (let row = 0; row < 4; row++) {
     for (let col = 0; col < 4; col++) {
       drawSquare(row * squareSize, col * squareSize);
@@ -23,7 +18,7 @@ function drawQuilt() {
 
 function drawSquare(x, y) {
   const color = getRandomColor();
-  ctx.fillStyle = "#FFFFFF"; // Square color is white
+  ctx.fillStyle = "#FFFFFF";
   ctx.fillRect(x, y, squareSize, squareSize);
 
   // Draw a half circle in a random corner of the square
